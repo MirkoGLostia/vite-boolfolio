7b87a26f2a3e95b5ac1109e9f20195df99503527
MirkoGLostia <giggi.torre@mail.com>
2023-08-22 15:25:58 +0200

    dati in console

---
+++ src/components/headerApp.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'headerApp',
+    mounted() {
+        axios.get('http://localhost:8000/api/v1/project-api').then(response => {
+            const data = response.data;
+            console.log(data);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+}
+</script>
+
+<template>
+    <h1>
+        hello world
+    </h1>
+</template>
